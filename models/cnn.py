b69061f334c1b7edb66290d0d6b14530dfb699ca
Pratik Singh <psingh22@ncsu.edu>
2018-11-15 02:08:26 -0500

    Tuned CNN 99.85%

--- models/cnn.py
+++ models/cnn.py
@@ -4,7 +4,7 @@
 
 from __future__ import print_function
 import keras
-from keras.layers import Dense, Flatten, Activation
+from keras.layers import Dense, Flatten, Activation, Dropout
 from keras.layers import Conv2D, MaxPooling2D
 from keras.models import Sequential
 import matplotlib.pylab as plt
@@ -13,10 +13,18 @@ import os
 import numpy as np
 import cv2
 
-NONE=0
-ROCK=1
-PAPER=2
-SCISSOR=3
+NONE=-1
+ROCK=0
+PAPER=1
+SCISSOR=2
+
+# image dimensions
+img_rows, img_cols = 300, 300
+
+batch_size = 64
+num_classes = 3
+epochs = 12
+img_dst='../img_dest'
 
 def getLabel(filename):
     if 'rock' in filename:
@@ -36,7 +44,8 @@ def getImageList(imageSourceFolder):
     labels = np.empty(noOfImages, dtype=int)
     for n in range(0, noOfImages):
         labels[n] = getLabel(onlyfiles[n])
-        img = cv2.imread( os.path.join(imageSourceFolder, onlyfiles[n]), 0 ).flatten() # gives 90000*1
+        img = cv2.imread( os.path.join(imageSourceFolder, onlyfiles[n]), 0 )
+        img = cv2.resize(img, (img_rows, img_cols)).flatten() # gives 10000*1
         for x in range(0, len(img)):
             images[n*img_rows*img_cols+x] = img[x]
     images = images.reshape(noOfImages, img_rows, img_cols)
@@ -53,17 +62,9 @@ class AccuracyHistory(keras.callbacks.Callback):
 history = AccuracyHistory()
 
 
-batch_size = 32
-num_classes = 4
-epochs = 10
-img_dst='../img_dest'
-
-# image dimensions
-img_rows, img_cols = 300, 300
-
 # load the images data set
 X,y = getImageList(img_dst)
-# The shape of X is (1920, 300, 300)
+# The shape of X is (1920, img_rows, img_cols)
 
 # split into test and train
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=42, stratify=y)
@@ -75,21 +76,6 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 
-#keras.backend.set_image_dim_ordering('th')
-
-model = Sequential()
-model.add(Conv2D(32, kernel_size=(5, 5), strides=(1, 1),activation='relu', input_shape=(img_rows, img_cols, 1)))
-model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-model.add(Conv2D(64, (5, 5), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Flatten())
-model.add(Dense(1000, activation='relu'))
-model.add(Dense(num_classes))
-model.add(Activation('softmax'))
-model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(), metrics=['accuracy'])
-
-model.summary()
-print(len(X_train))
 
 # The input shape that a CNN accepts should be in a specific format. If you are using Tensorflow, the format should be (batch, height, width, channels). If you are using Theano, the format should be (batch, channels, height, width).
 X_train = X_train.reshape(X_train.shape[0], img_rows, img_cols, 1)
@@ -102,11 +88,226 @@ X_test = X_test.astype('float32')
 X_train/=255
 X_test/=255
 
+'''
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3), strides=(1, 1), padding='valid', activation='relu', input_shape=(img_rows, img_cols, 1)))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(Conv2D(32, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+# Fully connected layer
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes))
+model.add(Activation('softmax'))
+model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(), metrics=['accuracy'])
+
+model.summary()
 model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(X_test, y_test),callbacks=[history])
 score = model.evaluate(X_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
-plt.plot(range(1, 11), history.acc)
+
+plt.plot(range(1, epochs+1), history.acc)
 plt.xlabel('Epochs')
 plt.ylabel('Accuracy')
 plt.show()
+
+# Test accuracy: 0.7223427332749164
+
+'''
+'''
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(5, 5), strides=(1, 1), padding='valid', activation='relu', input_shape=(img_rows, img_cols, 1)))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(Conv2D(32, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+# Fully connected layer
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes))
+model.add(Activation('softmax'))
+model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(), metrics=['accuracy'])
+
+model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(X_test, y_test),callbacks=[history])
+score = model.evaluate(X_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+# Test accuracy: 0.6782357195366662
+
+######## Dense(64)
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3), strides=(1, 1), padding='valid', activation='relu', input_shape=(img_rows, img_cols, 1)))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(Conv2D(32, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+# Fully connected layer
+model.add(Flatten())
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes))
+model.add(Activation('softmax'))
+model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(), metrics=['accuracy'])
+
+model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(X_test, y_test),callbacks=[history])
+score = model.evaluate(X_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+# Test accuracy: 0.6898047723204694
+
+######## Dense(256)
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3), strides=(1, 1), padding='valid', activation='relu', input_shape=(img_rows, img_cols, 1)))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(Conv2D(32, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+# Fully connected layer
+model.add(Flatten())
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes))
+model.add(Activation('softmax'))
+model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(), metrics=['accuracy'])
+
+model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(X_test, y_test),callbacks=[history])
+score = model.evaluate(X_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+#Test accuracy: 0.5885755605052885
+'''
+'''
+######## 3 CNN
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3), strides=(1, 1), padding='valid', activation='relu', input_shape=(img_rows, img_cols, 1)))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(Conv2D(32, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(32, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.5))
+# Fully connected layer
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes))
+model.add(Activation('softmax'))
+model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(), metrics=['accuracy'])
+
+model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(X_test, y_test),callbacks=[history])
+score = model.evaluate(X_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+#Test accuracy: 0.8264642082429501
+'''
+'''
+######## 2 64, 64 CNN
+model = Sequential()
+model.add(Conv2D(64, kernel_size=(3, 3), strides=(1, 1), padding='valid', activation='relu', input_shape=(img_rows, img_cols, 1)))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+# Fully connected layer
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes))
+model.add(Activation('softmax'))
+model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(), metrics=['accuracy'])
+
+model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(X_test, y_test),callbacks=[history])
+score = model.evaluate(X_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+#Test accuracy: 0.490238611735215
+
+
+######## BN
+model = Sequential()
+model.add(Conv2D(32, (3, 3), input_shape=(img_rows, img_cols, 1)))
+model.add(BatchNormalization(axis=-1))
+model.add(Activation('relu'))
+model.add(Conv2D(32, (3, 3)))
+model.add(BatchNormalization(axis=-1))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Conv2D(64,(3, 3)))
+model.add(BatchNormalization(axis=-1))
+model.add(Activation('relu'))
+model.add(Conv2D(64, (3, 3)))
+model.add(BatchNormalization(axis=-1))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Flatten())
+# Fully connected layer
+
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes))
+model.add(Activation('softmax'))
+model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(), metrics=['accuracy'])
+
+model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(X_test, y_test),callbacks=[history])
+score = model.evaluate(X_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+'''
+'''
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3), strides=(1, 1), padding='valid', activation='relu', input_shape=(img_rows, img_cols, 1)))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(Conv2D(32, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(32, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(32, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(32, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.5))
+# Fully connected layer
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes))
+model.add(Activation('softmax'))
+model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(), metrics=['accuracy'])
+
+model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(X_test, y_test),callbacks=[history])
+score = model.evaluate(X_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+#Test accuracy: 0.9898770788141721
+'''
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3), strides=(1, 1), padding='valid', activation='relu', input_shape=(img_rows, img_cols, 1)))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(Conv2D(32, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(32, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(32, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(32, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.5))
+# Fully connected layer
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes))
+model.add(Activation('softmax'))
+model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(), metrics=['accuracy'])
+
+model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(X_test, y_test),callbacks=[history])
+score = model.evaluate(X_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+#Test accuracy: 0.9985538684020245
+
+#todo
+# 1. how to save the model?
+# 2. load a pre-trained model and make predictions

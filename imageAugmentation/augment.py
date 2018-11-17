6749d2b96d19496300b42bd8f85b98d5c9f51204
Anirudha Tambolkar <atambolk@redhat.com>
2018-11-16 19:50:24 -0500

    image transformations fixed

--- imageAugmentation/augment.py
+++ imageAugmentation/augment.py
@@ -21,54 +21,29 @@ def writeImages(images):
 def transformImages(images):
     ia.seed(1)
 
-    image = images[0]
-    bbs = ia.BoundingBoxesOnImage([
-        ia.BoundingBox(x1=65, y1=100, x2=200, y2=150),
-        ia.BoundingBox(x1=150, y1=80, x2=200, y2=130)
-    ], shape=image.shape)
-
     seq = iaa.Sequential([
-        iaa.Multiply((1.2, 1.5)), # change brightness, doesn't affect BBs
+        iaa.Fliplr(0.5),
+        iaa.Crop(percent=(0, 0.1)), 
+        iaa.Sometimes(0.5,
+            iaa.GaussianBlur(sigma=(0, 0.5))
+        ),
+        iaa.ContrastNormalization((0.75, 1.5)),
+        iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05*255), per_channel=0.5),
+        iaa.Multiply((0.8, 1.2), per_channel=0.2),
         iaa.Affine(
-            translate_px={"x": 40, "y": 60},
-            scale=(0.5, 0.7)
-        ) # translate by 40/60px on x/y axis, and scale to 50-70%, affects BBs
-    ])
-
-    # Make our sequence deterministic.
-    # We can now apply it to the image and then to the BBs and it will
-    # lead to the same augmentations.
-    # IMPORTANT: Call this once PER BATCH, otherwise you will always get the
-    # exactly same augmentations for every batch!
-    seq_det = seq.to_deterministic()
-
-    # Augment BBs and images.
-    # As we only have one image and list of BBs, we use
-    # [image] and [bbs] to turn both into lists (batches) for the
-    # functions and then [0] to reverse that. In a real experiment, your
-    # variables would likely already be lists.
-    image_aug = seq_det.augment_images([image])[0]
-    bbs_aug = seq_det.augment_bounding_boxes([bbs])[0]
-
-    # print coordinates before/after augmentation (see below)
-    # use .x1_int, .y_int, ... to get integer coordinates
-    for i in range(len(bbs.bounding_boxes)):
-        before = bbs.bounding_boxes[i]
-        after = bbs_aug.bounding_boxes[i]
-        print("BB %d: (%.4f, %.4f, %.4f, %.4f) -> (%.4f, %.4f, %.4f, %.4f)" % (
-            i,
-            before.x1, before.y1, before.x2, before.y2,
-            after.x1, after.y1, after.x2, after.y2)
+            scale={"x": (0.8, 1.2), "y": (0.8, 1.2)},
+            translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)},
+            rotate=(-25, 25),
+            shear=(-8, 8)
         )
+    ], random_order=True)
 
-    # image with BBs before/after augmentation (shown below)
-    image_before = bbs.draw_on_image(image, thickness=2)
-    image_after = bbs_aug.draw_on_image(image_aug, thickness=2, color=[0, 0, 255])
-
+    return seq.augment_images(images)
 
 def main():
     images = readImages("./inputImages")
-    
+    augImages = transformImages(images)
+    writeImages(augImages)
 
 
 main()
